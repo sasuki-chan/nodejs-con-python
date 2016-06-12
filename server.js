f084747d75641fa5e9e77da8c01eb15351244a8d
sasuki-chan <itachi_sasuke_uchiha_@hotmail.com>
2016-06-12 16:50:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
+// dependencias del proyecto,tienes que tener instaladas antes para que corra tu codigo
 var express = require('express');
 var app = express();
+var formidable = require('formidable');
 var server = require('http').createServer(app);
-var bodyParser = require('body-parser');
-var session = require('express-session');
 var port = process.env.PORT ||8080;
 server.listen(port);
-var fs = require('fs');
-
 GLOBAL.PythonShell = require('python-shell');
+// Hasta la linea anterior terminan las dependencias
+
+// Definimos el archivo a python a llamar mas adelante cuando se obtenga una imagen del usuario tomada desde el navegador
 GLOBAL.pyshell = new PythonShell('mostrar_imagen.py');
 
-var formidable = require('formidable');
-var util = require('util');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 app.get('/', function(req,res){
-		app.use(express.static(__dirname + '/public'));
+		app.use(express.static(__dirname + '/public')); // con esta linea servimos la pagina
 		res.sendfile(__dirname, + 'basic.html');
 });
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -31,26 +28,29 @@ function upload(req, res){
     var form = new formidable.IncomingForm();
     var files = [];
     form.uploadDir = "upload";
-    console.log("Se ha recibido el formulario");
+    console.log("Se ha recibido la imagen");
 
     form.parse(req, function(err, fields, files) {
+      console.log(files.webcam.path);  // aqui puedes ver el path donde guarda la imagen nodejs
+          //este es el argumento que necesita python para encontrar la imagen
 
     var options = {
          mode: 'text',
          pythonOptions: ['-u'],
-         args: ['hola', 'mundo', 'xD']
+         args: [files.webcam.path]  // Aqui pasamos la ruta de la imagen al python
         };
 
      
 	  PythonShell.run('mostrar_imagen.py', options, function(err, result) {
 			if (err) throw err;
-			console.log(result)
-			console.log('finished');
+			console.log('result: %j', result); // aqui vemos los resultados de la ejecucion del codigo
+			console.log('finalizado el programa'); // Este mensaje lo muestra cuando el programa se cierra
 });
-    });
+    });  // termina el codigo de form.parse(req,function(err,fields,files){
     return;
   }
-}
-//////////////////////////////////////////////////////
+}     // cierra la llave de la funcion upload
 
-console.log('Servidor corriendo en https:localhost:8080');
+//////////////////////////////////////////////////////////////////////////////
+
+console.log('Servidor corriendo en localhost:8080');
