5fbeab8c2eeda3ec6223793bf0b2fa2a45e63f4d
sasuki-chan <itachi_sasuke_uchiha_@hotmail.com>
2016-06-12 16:36:24 -0600

    Update mostrar_imagen.py

--- mostrar_imagen.py
+++ mostrar_imagen.py
@@ -4,9 +4,9 @@ import numpy
 import cv2
 import sys
 
-for v in sys.argv[1:]:
-  print v
-  
-imagen= cv2.imread("image/python.jpeg")
-cv2.imshow("Ventana de imagen", imagen)
-cv2.waitKey(0)
+for v in sys.argv[1:]:		                # Ciclo para llamar los argumentos pasados por nodejs al script de python
+  path = sys.argv[1]                      # El primer argumento que le recibe de nodejs es la ruta del archivo donde nodejs guardo la imagen
+  print('La variable path es:  ',path)    # Aqui te muestra en la consola donde esta ubicada la imagen
+  imagen= cv2.imread(path)                # Leemos la imagen basada en el path que recibio por parte de nodejs
+  cv2.imshow("Ventana de imagen", imagen) # Mostramos la imagen en una ventana aparte
+  cv2.waitKey(5000)                       # Tiempo programado en milisegundos para cerrar la imagen automaticamente
